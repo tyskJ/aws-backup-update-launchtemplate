e4efca5290ca0892deefab00e2c91df928a81da0
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-05 15:25:48 +0900

    add iam module

--- code/tf/envs/main.tf
+++ code/tf/envs/main.tf
@@ -2,6 +2,7 @@
 # ║ Ep.01 Launch Template Update Solution - Terraform main.tf module                                                                          ║
 # ╠═════════════════╤═══════════════════════════════════╤═════════════════════════════════════════════════════════════════════════════════════╣
 # ║ nw              │ ./modules/vpc_subnet              │ invoke network module.                                                              ║
+# ║ iam             │ ./modules/iam                     │ invoke iam module.                                                                  ║
 # ╚═════════════════╧═══════════════════════════════════╧═════════════════════════════════════════════════════════════════════════════════════╝
 
 module "nw" {
@@ -9,4 +10,13 @@ module "nw" {
 
   vpc_map    = { "name" = "${local.env}-vpc", "cidr" = "10.0.0.0/16", "dnshost" = "true", "dnssupport" = "true" }
   subnet_map = { "name" = "${local.env}-private-subnet", "cidr" = "10.0.0.0/24", "az_name" = "ap-northeast-1a" }
+}
+
+module "iam" {
+  source = "../modules/iam"
+
+  ec2_role_name = "${local.env}-ec2-role"
+  lambda_role_name = "${local.env}-lambda-role"
+  backup_role_name = "${local.env}-backup-role"
+  ltupdate_policy_name = "${local.env}-ltupdate-policy"
 }
