cdbc843b03f90143cb1ba9acc28440dd13e6adc7
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-05 19:51:58 +0900

    add ec2 module

--- code/tf/envs/main.tf
+++ code/tf/envs/main.tf
@@ -5,6 +5,7 @@
 # ║ iam             │ ./modules/iam                     │ invoke iam module.                                                                  ║
 # ║ kms             │ ./modules/kms                     │ invoke kms module.                                                                  ║
 # ║ backup          │ ./modules/awsbackup               │ invoke awsbackup module.                                                            ║
+# ║ ec2             │ ./modules/ec2                     │ invoke ec2 module.                                                                  ║
 # ╚═════════════════╧═══════════════════════════════════╧═════════════════════════════════════════════════════════════════════════════════════╝
 
 module "nw" {
@@ -35,4 +36,17 @@ module "backup" {
 
   bkvault_name = "${local.env}-bkvault"
   kms_key_arn  = module.kms.backup_kms_key_arn
+}
+
+module "ec2" {
+  source = "../modules/ec2"
+
+  private_key_file_name     = local.private_key_file
+  key_name                  = local.keypair_name
+  securitygroup_name        = "${local.env}-ec2-sg"
+  vpc_id                    = module.nw.vpc_id
+  subnet_id                 = module.nw.subnet_id
+  kms_key_id                = module.kms.ec2_kms_key_id
+  ec2_instance_profile_name = module.iam.ec2_instance_profile_name
+  ec2_map                   = { "name" = "${local.env}-ec2", "instancetype" = "t2.micro", "volname" = "${local.env}-ebs-root", "volumesize" = "30" }
 }
