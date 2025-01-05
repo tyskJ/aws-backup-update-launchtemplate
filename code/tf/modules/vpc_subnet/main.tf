5a45b0be2749aeecfb0276b96efb6138f4814a85
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-05 13:34:18 +0900

    create nw modules

---
+++ code/tf/modules/vpc_subnet/main.tf
@@ -0,0 +1,24 @@
+# ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+# ║ Ep.01 Launch Template Update Solution - Terraform main.tf resource                                                                        ║
+# ╠═════════════════╤═══════════════════════════════════╤═════════════════════════════════════════════════════════════════════════════════════╣
+# ║ vpc             │ aws_vpc                           │ VPC.                                                                                ║
+# ║ subnet          │ aws_subnet                        │ Subnet.                                                                             ║
+# ╚═════════════════╧═══════════════════════════════════╧═════════════════════════════════════════════════════════════════════════════════════╝
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_map.cidr
+  enable_dns_hostnames = var.vpc_map.dnshost
+  enable_dns_support   = var.vpc_map.dnssupport
+  tags = {
+    Name = var.vpc_map.name
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_map.cidr
+  availability_zone = var.subnet_map.az_name
+  tags = {
+    Name = var.subnet_map.name
+  }
+}
---
+++ code/tf/modules/vpc_subnet/variable.tf
@@ -0,0 +1,16 @@
+# ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+# ║ Ep.01 Launch Template Update Solution - Terraform variable.tf variable                                                                    ║
+# ╠═════════════════╤═══════════════════════════════════╤═════════════════════════════════════════════════════════════════════════════════════╣
+# ║ vpc_map         │ map(string)                       │ VPC settings map.                                                                   ║
+# ║ subnet_map      │ map(string)                       │ Subnet settings map.                                                                ║
+# ╚═════════════════╧═══════════════════════════════════╧═════════════════════════════════════════════════════════════════════════════════════╝
+
+variable "vpc_map" {
+  type        = map(string)
+  description = "VPC settings map."
+}
+
+variable "subnet_map" {
+  type        = map(string)
+  description = "Subnet settings map."
+}
