341eae802bcb3c5c425c9d64fd9267444e049743
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-05 16:12:59 +0900

    rename variable to variables

--- code/tf/modules/kms/variables.tf
+++ code/tf/modules/kms/variables.tf
@@ -1,28 +1,16 @@
 # ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-# ║ Ep.01 Launch Template Update Solution - Terraform variable.tf variable                                                                           ║
+# ║ Ep.01 Launch Template Update Solution - Terraform variables.tf variable                                                                           ║
 # ╠════════════════════════╤═══════════════════════════════════╤═════════════════════════════════════════════════════════════════════════════════════╣
-# ║ ec2_role_name          │ string                            │ EC2 IAM Role name.                                                                  ║
-# ║ lambda_role_name       │ string                            │ Lambda IAM Role name.                                                               ║
-# ║ backup_role_name       │ string                            │ Backup IAM Role name.                                                               ║
-# ║ ltupdate_policy_name   │ string                            │ ltupdate IAM Policy name.                                                           ║
+# ║ ec2_cmk_name           │ string                            │ EC2 CMK name.                                                                       ║
+# ║ backup_cmk_name        │ string                            │ AWS Backup CMK name.                                                                ║
 # ╚════════════════════════╧═══════════════════════════════════╧═════════════════════════════════════════════════════════════════════════════════════╝
 
-variable "ec2_role_name" {
+variable "ec2_cmk_name" {
   type        = string
-  description = "EC2 IAM Role name."
+  description = "EC2 CMK name."
 }
 
-variable "lambda_role_name" {
+variable "backup_cmk_name" {
   type        = string
-  description = "Lambda IAM Role name."
-}
-
-variable "backup_role_name" {
-  type        = string
-  description = "Backup IAM Role name."
-}
-
-variable "ltupdate_policy_name" {
-  type        = string
-  description = "ltupdate IAM Policy name."
+  description = "AWS Backup CMK name."
 }
