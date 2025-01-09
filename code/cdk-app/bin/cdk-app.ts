7560d39f51633f6716c35b9623eb775e072f3fb1
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-09 21:27:05 +0900

    add parameter props

--- code/cdk-app/bin/cdk-app.ts
+++ code/cdk-app/bin/cdk-app.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { CdkAppStack } from "../lib/cdk-app-stack";
+import { CdkAppStack } from "../lib/stack/cdk-app-stack";
+import { devParameter } from "../parameter";
 
 const app = new cdk.App();
-new CdkAppStack(app, "CdkAppStack", {});
+const stack = new CdkAppStack(app, "CdkAppStack", devParameter);
+
+cdk.Tags.of(stack).add("app", devParameter.AppName);
