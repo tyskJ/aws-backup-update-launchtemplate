7aaea21e5ee9d4da1747a2814cd149c55ce53334
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-10 00:44:08 +0900

    create kms construct

---
+++ code/cdk-app/lib/construct/kms.ts
@@ -0,0 +1,47 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+║ Ep.01 Launch Template Update Solution - Cloud Development Kit kms.ts                                                                     ║
+╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣
+║ This construct creates an L2 Construct KMS Key and Alias.                                                                                ║
+╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+*/
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { kmsInfo } from "../../parameter";
+import * as kms from "aws-cdk-lib/aws-kms";
+
+export interface KmsInfo {
+  ebsCmk: kmsInfo;
+  backupCmk: kmsInfo;
+}
+
+export class Kms extends Construct {
+  public readonly ebsCmk: kms.Key;
+  public readonly backupCmk: kms.Key;
+
+  constructor(scope: Construct, id: string, props: KmsInfo) {
+    super(scope, id);
+
+    this.ebsCmk = new kms.Key(this, props.ebsCmk.id, {
+      alias: props.ebsCmk.alias,
+      description: props.ebsCmk.description,
+      enableKeyRotation: props.ebsCmk.keyRotation,
+      pendingWindow: cdk.Duration.days(props.ebsCmk.pendingWindow),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    for (const tag of props.ebsCmk.tags) {
+      cdk.Tags.of(this.ebsCmk).add(tag.key, tag.value);
+    }
+
+    this.backupCmk = new kms.Key(this, props.backupCmk.id, {
+      alias: props.backupCmk.alias,
+      description: props.backupCmk.description,
+      enableKeyRotation: props.backupCmk.keyRotation,
+      pendingWindow: cdk.Duration.days(props.backupCmk.pendingWindow),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    for (const tag of props.backupCmk.tags) {
+      cdk.Tags.of(this.backupCmk).add(tag.key, tag.value);
+    }
+  }
+}
