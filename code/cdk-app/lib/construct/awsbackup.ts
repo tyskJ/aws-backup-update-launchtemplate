4ef02bef3e51c8693aba652faed4afb270a3b078
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-11 13:07:20 +0900

    create backup construct

---
+++ code/cdk-app/lib/construct/awsbackup.ts
@@ -0,0 +1,34 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+║ Ep.01 Launch Template Update Solution - Cloud Development Kit awsbackup.ts                                                               ║
+╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣
+║ This construct creates an L2 Construct AWS Backup Vault.                                                                                 ║
+╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+*/
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as backup from "aws-cdk-lib/aws-backup";
+import * as kms from "aws-cdk-lib/aws-kms";
+import { bkvaultInfo } from "../../parameter";
+
+export interface BackupProps {
+  bkVault: bkvaultInfo;
+  backupCmk: kms.Key;
+}
+
+export class AwsBackup extends Construct {
+  constructor(scope: Construct, id: string, props: BackupProps) {
+    super(scope, id);
+
+    const bkvault = new backup.BackupVault(this, props.bkVault.id, {
+      backupVaultName: props.bkVault.name,
+      encryptionKey: props.backupCmk,
+      removalPolicy: props.bkVault.removalPolicy
+        ? cdk.RemovalPolicy.DESTROY
+        : cdk.RemovalPolicy.RETAIN,
+    });
+    for (const tag of props.bkVault.tags) {
+      cdk.Tags.of(bkvault).add(tag.key, tag.value);
+    }
+  }
+}
