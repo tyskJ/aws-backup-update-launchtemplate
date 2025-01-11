4a49652b13f4ee40b5f6e4fd26d6751287160519
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-11 19:19:55 +0900

    update stack name

--- code/cdk-app/bin/cdk-app.ts
+++ code/cdk-app/bin/cdk-app.ts
@@ -11,6 +11,6 @@ import { CdkAppStack } from "../lib/stack/cdk-app-stack";
 import { devParameter } from "../parameter";
 
 const app = new cdk.App();
-const stack = new CdkAppStack(app, "CdkAppStack", devParameter);
+const stack = new CdkAppStack(app, "Ep01Stack", devParameter);
 
 cdk.Tags.of(stack).add("app", devParameter.AppName);
