f7af0bbd2cdb27fa6a7c9355556de9bb68ebe833
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-01-11 12:50:18 +0900

    create iam construct

--- code/cdk-app/lib/construct/iam.ts
+++ code/cdk-app/lib/construct/iam.ts
@@ -9,32 +9,45 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as iam from "aws-cdk-lib/aws-iam";
 import { iamPolicyInfo } from "../../parameter";
+import { iamRoleInfo } from "../../parameter";
 import * as fs from "fs";
 import * as path from "path";
 
 export interface IamProps {
   ltupdatePolicy: iamPolicyInfo;
+  lambdaRole: iamRoleInfo;
+  backupRole: iamRoleInfo;
+  ec2Role: iamRoleInfo;
 }
 
 export class Iam extends Construct {
-  public readonly backupRole: iam.Role;
   public readonly lambdaRole: iam.Role;
+  public readonly backupRole: iam.Role;
   public readonly ec2Role: iam.Role;
 
   constructor(scope: Construct, id: string, props: IamProps) {
     super(scope, id);
 
     const ltupdatePolicy = this.createIamPolicy(this, props.ltupdatePolicy);
+
+    const lambdaRole = this.createIamRole(this, props.lambdaRole, [
+      ltupdatePolicy,
+    ]);
+    const backupRole = this.createIamRole(this, props.backupRole);
+    const ec2Role = this.createIamRole(this, props.ec2Role);
   }
   /*
   ╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
   ║ Method (private)                                                                                               ║
   ╠═══════════════════════════╤═════════════════════════╤══════════════════════════════════════════════════════════╣
-  ║ createIamPolicy           │ iam.Policy              │ Method to create IAM Managed Policy for L2 constructs.   ║
+  ║ createIamPolicy           │ iam.ManagedPolicy       │ Method to create IAM Managed Policy for L2 constructs.   ║
   ║ createIamRole             │ iam.Role                │ Method to create IAM Role for L2 constructs.             ║
   ╚═══════════════════════════╧═════════════════════════╧══════════════════════════════════════════════════════════╝
   */
-  private createIamPolicy(scope: Construct, policyInfo: iamPolicyInfo) {
+  private createIamPolicy(
+    scope: Construct,
+    policyInfo: iamPolicyInfo
+  ): iam.ManagedPolicy {
     const filePath = path.join(
       `${__dirname}`,
       "../json/policy/",
@@ -46,5 +59,33 @@ export class Iam extends Construct {
       description: policyInfo.description,
       document: iam.PolicyDocument.fromJson(jsonData),
     });
+    return iamManagedPolicy;
+  }
+
+  private createIamRole(
+    scope: Construct,
+    roleInfo: iamRoleInfo,
+    managedPolicy?: iam.ManagedPolicy[]
+  ) {
+    const iamRole = new iam.Role(scope, roleInfo.id, {
+      roleName: roleInfo.roleName,
+      description: roleInfo.description,
+      assumedBy: new iam.ServicePrincipal(roleInfo.assumed),
+    });
+    if (roleInfo.awsManagedPolicyAdd) {
+      for (const amp of roleInfo.awsManagedPolicyName!) {
+        iamRole.addManagedPolicy(
+          iam.ManagedPolicy.fromAwsManagedPolicyName(amp.policyName)
+        );
+      }
+    }
+    if (roleInfo.customManagedPolicyAdd) {
+      for (const cmp of managedPolicy!) {
+        iamRole.addManagedPolicy(cmp);
+      }
+    }
+    for (const tag of roleInfo.tags) {
+      cdk.Tags.of(iamRole).add(tag.key, tag.value);
+    }
   }
 }
